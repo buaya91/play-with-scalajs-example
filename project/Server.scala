b5fa54db84685e074ce69aee00cfdb6500894495
Qingwei <l.q.wei91@gmail.com>
2016-12-11 10:48:44 +0000

    Fix dependencies

--- project/Server.scala
+++ project/Server.scala
@@ -5,6 +5,7 @@ object Server {
     lazy val akkaV = "2.4.12"
     lazy val scalajsScriptV = "1.1.0"
     lazy val logbackV = "1.1.7"
+    lazy val scalatestV = "3.0.0"
     lazy val scalacheckV = "1.13.0"
   }
 
@@ -14,6 +15,7 @@ object Server {
       "ch.qos.logback"    % "logback-core"         % Versions.logbackV,
       "com.typesafe.akka" %% "akka-stream-testkit" % Versions.akkaV % "test",
       "com.typesafe.akka" %% "akka-testkit"        % Versions.akkaV % "test",
+      "org.scalatest"     %% "scalatest"           % Versions.scalatestV % "test",
       "org.scalacheck"    %% "scalacheck"          % Versions.scalacheckV % "test"
     )
   )
