85b31fe9b70334e5c4ba460831bea97a57be07ab
Qingwei <l.q.wei91@gmail.com>
2017-01-14 12:45:54 +0000

    Fix minor bugs

--- client/src/main/scala/client/BrowserSnakeGame.scala
+++ client/src/main/scala/client/BrowserSnakeGame.scala
@@ -13,25 +13,28 @@ import monix.execution.Scheduler.Implicits.global
 
 object BrowserSnakeGame extends JSApp {
 
-  def onSubmitName() = {
+  private def onSubmitName(): Boolean = {
     val name = document.getElementById("username-input").asInstanceOf[HTMLInputElement].value
-    DefaultWSSource.request(JoinGame(name))
-    println("Joined!")
-    true
+
+    if (name != null && name != "") {
+      DefaultWSSource.request(JoinGame(name))
+      true
+    } else {
+      false
+    }
   }
 
   def initDom() = {
     JSFacade
       .JQueryStatic("#username-modal")
       .modal(Dynamic.literal(autofocus = true, onHide = () => onSubmitName()))
+      .modal("setting", "closable", false)
       .modal("show")
 
     val nameInput = document.getElementById("username-input").asInstanceOf[HTMLInputElement]
     document.addEventListener("keydown", (ev: KeyboardEvent) => {
       if (ev.keyCode == 13) {
-        JSFacade
-          .JQueryStatic("#username-modal")
-          .modal("hide")
+        JSFacade.JQueryStatic("#username-modal").modal("hide")
       }
     })
   }
